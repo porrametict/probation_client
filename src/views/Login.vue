d3b74ad63741866fa13c5bdee5f1fbaa5f748308
porrametict <porrametict@gmal.com>
2020-03-27 20:38:20 +0700

    re design login page for mobile first

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,58 +5,39 @@
                     class="fill-height"
                     fluid
             >
-                <v-row
-                        align="center"
-                        justify="center"
-                >
-                    <v-col
-                            cols="12"
-                            sm="8"
-                            md="4"
-                    >
-                        <v-card class="elevation-12">
-                            <v-toolbar
-                                    color="primary"
-                                    dark
-                                    flat
-                            >
-                                <v-toolbar-title>Login form</v-toolbar-title>
-                                <v-spacer/>
-
-                            </v-toolbar>
-                            <v-card-text>
-                                <v-form>
-                                    <v-text-field
-                                            label="Login"
-                                            name="login"
-                                            prepend-icon="mdi-account-box"
-                                            type="text"
-                                            v-model="form.username"
-                                    />
-
-                                    <v-text-field
-                                            id="password"
-                                            label="Password"
-                                            name="password"
-                                            prepend-icon="mdi-lock-question"
-                                            type="password"
-                                            v-model="form.password"
-                                            @keypress.13="login"
-                                    />
-                                </v-form>
-                            </v-card-text>
-                            <v-card-actions>
-                                <v-spacer/>
-                                <v-btn color="primary" @click="login()">Login</v-btn>
-                            </v-card-actions>
-                        </v-card>
-                    </v-col>
+                <v-row class="d-flex justify-center flex-column flex-md-row align-md-center ma-3 ma-md-0">
+                    <div class="mx-md-12">
+                    <v-row class="d-flex justify-center flex-column align-center">
+                        <v-icon class="display-4" color="primary">
+                            mdi-owl
+                        </v-icon>
+                        <p class="blue--text title ma-0">PROBATION</p>
+                    </v-row>
+                    </div>
+                    <div class="mx-md-12">
+                        <v-text-field
+                                label="Login"
+                                name="login"
+                                prepend-icon="mdi-account-box"
+                                type="text"
+                                v-model="form.username"
+                        />
+                        <v-text-field
+                                id="password"
+                                label="Password"
+                                name="password"
+                                prepend-icon="mdi-lock-question"
+                                type="password"
+                                v-model="form.password"
+                                @keypress.13="login"
+                        />
+                        <v-btn color="primary" @click="login()" block large>Login</v-btn>
+                    </div>
                 </v-row>
             </v-container>
         </v-content>
     </v-app>
 </template>
-
 <script>
     export default {
         name: "Login",
@@ -83,7 +64,7 @@
         }
     }
 </script>
-
 <style scoped>
-
 </style>
+
+
