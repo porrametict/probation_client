8ddee154679f3517b3859314ccc417b64f249fb0
ford19982541 <33016279+ford19982541@users.noreply.github.com>
2020-03-28 14:59:47 +0700

    Edit template

--- src/views/template.vue
+++ src/views/template.vue
@@ -1,21 +1,26 @@
 <template>
     <v-app id="inspire">
-        <v-navigation-drawer app
-                class="deep-purple accent-4"
-                dark
-                permanent
+        <v-toolbar app
+                src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
         >
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Vuetify</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>mdi-export</v-icon>
+            </v-btn>
+        </v-toolbar>
+        <v-navigation-drawer app  permanent>
             <v-list>
-                <v-list-item
-
-                        link
-                >
+                <v-list-item link>
                     <v-list-item-icon>
-                        <v-icon></v-icon>
+                        <v-icon>mdi-steam</v-icon>
                     </v-list-item-icon>
-
                     <v-list-item-content>
-                        <v-list-item-title></v-list-item-title>
+                        <v-list-item-title>Hello</v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
             </v-list>
@@ -26,46 +31,6 @@
                 </div>
             </template>
         </v-navigation-drawer>
-        <v-app-bar app
-                color="deep-purple accent-4"
-                dense
-                dark
-        >
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-            <v-toolbar-title>Page title</v-toolbar-title>
-
-            <v-spacer></v-spacer>
-
-            <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-            </v-btn>
-
-            <v-btn icon>
-                <v-icon>mdi-magnify</v-icon>
-            </v-btn>
-
-            <v-menu
-                    left
-                    bottom
-            >
-                <template v-slot:activator="{ on }">
-                    <v-btn icon v-on="on">
-                        <v-icon>mdi-dots-vertical</v-icon>
-                    </v-btn>
-                </template>
-
-                <v-list>
-                    <v-list-item
-                            v-for="n in 5"
-                            :key="n"
-                            @click="() => {}"
-                    >
-                        <v-list-item-title>Option {{ n }}</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
-        </v-app-bar>
         <v-content>
             <router-view></router-view>
         </v-content>
