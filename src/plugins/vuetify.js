df1bec1adc04c48b7eac34c0b8c5e360a4e28bba
ford19982541 <33016279+ford19982541@users.noreply.github.com>
2020-03-31 14:05:20 +0700

    Change Color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,14 +3,18 @@ import Vuetify from 'vuetify/lib';
 import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
-
+const theme = {
+    primary: '#1E88E5',
+    secondary: '#9C27b0',
+    accent: '#9C27b0',
+    info: '#00CAE3',
+    success : colors.green.darken1,
+    danger : colors.red.darken2
+}
 export default new Vuetify({
     theme: {
         themes : {
-            light : {
-                success : colors.green.darken1,
-                danger : colors.red.darken2
-            }
+            light : theme
         }
 
     }
