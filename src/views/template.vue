b91beab38a63c2701613edfcfd3c00cdd5e0721b
ford19982541 <33016279+ford19982541@users.noreply.github.com>
2020-03-31 14:05:00 +0700

    Update Menu

--- src/views/template.vue
+++ src/views/template.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-app>
+    <v-app >
         <core-app-bar/>
         <core-drawer/>
         <core-view/>
@@ -7,13 +7,11 @@
 </template>
 
 <script>
-    import CoreView from "./Core/View";
     export default {
         name: "template",
         components: {
             CoreAppBar: () => import('./Core/AppBar'),
             CoreDrawer: () => import('./Core/Drawer'),
-            // DashboardCoreSettings: () => import('./components/core/Settings'),
             CoreView: () => import('./Core/View'),
         }
     }
