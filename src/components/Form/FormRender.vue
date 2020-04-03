522637c1f84ef57d7b9178e8a8784f6bb5665e2c
porrametict <porrametict@gmal.com>
2020-04-03 12:29:20 +0700

    new Form Component Structure

---
+++ src/components/Form/FormRender.vue
@@ -0,0 +1,109 @@
+<template>
+    <v-container>
+        <div class="d-flex justify-content-between">
+            <h3 class="title text-md-left text-center">
+                ข้อมูล
+            </h3>
+            <div v-if="!read_only">
+                <v-btn color="primary"
+                       class="white--text font-weight-bold"
+                       @click="saveForm"
+                >
+                    บันทึกข้อมูล
+                </v-btn>
+            </div>
+        </div>
+        <v-divider></v-divider>
+        <div v-if="assignment">
+            <SurveyFormRender
+                    ref="s_form"
+                    :assignment="assignment"
+                    :survey_data="survey_data"
+                    :form_type="assignment.form_type"
+                    :form_full="form_full"
+                    :read_only="read_only"
+                    @oncomplete="on_form_complete"
+                    @on_save="on_form_save"
+            >
+            </SurveyFormRender>
+        </div>
+    </v-container>
+</template>
+
+<script>
+    import SurveyFormRender from "./SurveyFormRender";
+
+    export default {
+        name: "assignment_form",
+        props: {
+            read_only: {
+                type: Boolean,
+                default: false
+            },
+            form_full: {
+                type: Boolean,
+                default: false
+            },
+            assignment: {
+                type: Object,
+                require: true
+            }
+        },
+        components: {SurveyFormRender},
+        data() {
+            return {
+                survey_data: null
+            }
+        },
+        async mounted() {
+            this.loadData()
+        },
+        methods: {
+            async loadData() {
+                if (this.assignment.form_type === 1 && this.assignment.during_probation_form_data) {
+                    this.survey_data = this.assignment.during_probation_form_data.form
+                } else if (this.assignment.form_type === 2 && this.assignment.after_probation_form_data) {
+                    this.survey_data = this.assignment.after_probation_form_data.form
+                }
+            },
+            async on_form_complete(e) {
+                this.save(e)
+            },
+            on_form_save(e) {
+                this.save(e)
+            },
+            async save(e) {
+                let saveMethod = "postForm"
+                let form_type = null
+
+                if (this.assignment.form_type === 1) {
+                    form_type = 'during_probation_form'
+                } else if (this.assignment.form_type === 2) {
+                    form_type = 'after_probation_form'
+                }
+                if (this.assignment[`${form_type}_data`]) {
+                    if (this.assignment[`${form_type}_data`].id) {
+                        saveMethod = "putForm"
+                    }
+                } else {
+                    this.assignment[`${form_type}_data`] = {}
+                    this.assignment[`${form_type}_data`].assignment = this.assignment.id
+                }
+                this.assignment[`${form_type}_data`].form = e.form
+
+                let data = await this.$store.dispatch(`${form_type}/${saveMethod}`, this.assignment[`${form_type}_data`])
+
+                if (data) {
+                    this.$emit('save_success', data)
+                }
+            },
+            saveForm() {
+                this.$refs.s_form.save_data()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Volunteer/Assignment/form.vue
+++ src/views/Volunteer/Assignment/form.vue
@@ -1,48 +1,22 @@
 <template>
-    <v-container>
-        <div class="d-flex justify-content-between">
-            <h3 class="title text-md-left text-center">
-                เก็บข้อมูล
-            </h3>
-            <div>
-                <v-btn color="primary"
-                       class="white--text font-weight-bold"
-                       @click="saveForm"
-                >
-                    บันทึกข้อมูล
-                </v-btn>
-            </div>
-        </div>
-        <v-divider></v-divider>
-        <div v-if="assignment">
-            <DuringProbationForm
-                    ref="d_form"
-                    v-if="assignment.form_type === 1"
-                    :assignment="assignment"
-                    :survey_data="survey_data"
-                    @oncomplete="on_form_complete"
-                    @on_save="on_form_save"
-            ></DuringProbationForm>
-
-            <AfterProbationForm
-                    ref="a_form"
-                    v-else-if="assignment.form_type === 2"
-                    :assignment="assignment"
-                    :survey_data="survey_data"
-                    @oncomplete="on_form_complete"
-                    @on_save="on_form_save"
-            ></AfterProbationForm>
-        </div>
+    <v-container v-if="assignment">
+        <FormRender
+                :assignment="assignment"
+                @save_success="save_form_success"
+        >
+        </FormRender>
     </v-container>
 </template>
 
 <script>
-    import AfterProbationForm from "../../../components/Form/AfterProbationForm";
-    import DuringProbationForm from "../../../components/Form/DuringProbationForm";
+
+    import FormRender from "../../../components/Form/FormRender";
 
     export default {
         name: "assignment_form",
-        components: {DuringProbationForm, AfterProbationForm},
+        components: {
+            FormRender
+        },
         data() {
             return {
                 assignment: null,
@@ -56,50 +30,9 @@
             async loadData() {
                 let id = this.$route.params.id
                 this.assignment = await this.$store.dispatch('assignment/getAssignmentById', id)
-                if (this.assignment.form_type === 1 && this.assignment.during_probation_form_data) {
-                    this.survey_data = this.assignment.during_probation_form_data.form
-                } else if (this.assignment.form_type === 2 && this.assignment.after_probation_form_data) {
-                    this.survey_data = this.assignment.after_probation_form_data.form
-                }
             },
-            async on_form_complete(e) {
-                this.save(e)
-            },
-            on_form_save(e) {
-                this.save(e)
-            },
-            async save(e) {
-                let saveMethod = "postForm"
-                let form_type = null
-
-                if (this.assignment.form_type === 1) {
-                    form_type = 'during_probation_form'
-                } else if (this.assignment.form_type === 2) {
-                    form_type = 'after_probation_form'
-                }
-                if (this.assignment[`${form_type}_data`]) {
-                    if (this.assignment[`${form_type}_data`].id) {
-                        saveMethod = "putForm"
-                    }
-                } else {
-                    this.assignment[`${form_type}_data`] = {}
-                    this.assignment[`${form_type}_data`].assignment = this.assignment.id
-                }
-                this.assignment[`${form_type}_data`].form = e.form
-
-                let data = await this.$store.dispatch(`${form_type}/${saveMethod}`, this.assignment[`${form_type}_data`])
-
-                if (data) {
-                    await this.$router.push({name: 'ViewAssignmentDetail', params: {id: this.assignment.id}})
-                }
-            },
-            saveForm() {
-                if (this.assignment.form_type === 1) {
-                    this.$refs.d_form.save_data()
-                } else if (this.assignment.form_type === 2) {
-                    this.$refs.a_form.save_data()
-                }
-
+            save_form_success(e) {
+                console.log(e)
             }
         }
     }
