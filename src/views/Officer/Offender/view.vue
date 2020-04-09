7e639fcf782f7ffa771071f924183f270c499419
porrametict <porrametict@gmal.com>
2020-04-09 21:08:01 +0700

    offender view page

--- src/views/Officer/Offender/view.vue
+++ src/views/Officer/Offender/view.vue
@@ -1,12 +1,140 @@
 <template>
-    <v-container>
-        {{$route.params.id}}
+    <v-container v-if="offender">
+        <div>
+            <p class="title">รายละเอียด</p>
+        </div>
+        <!--            main-->
+        <div>
+            <!--                offender-->
+            <data-list-render :data="offender_data_render">
+                <template v-slot:header>
+                    <div class="white--text">ผู้กระทำผิด</div>
+                    <div class="title white--text mb-2">
+                        <p class="ma-0">
+                            {{offender.o_personal_id}}
+                        </p>
+                        <p class="ma-0">
+                            {{offender.o_first_name + " "
+                            +offender.o_last_name}}
+                        </p>
+                    </div>
+                </template>
+            </data-list-render>
+            <data-list-render :data="offender_address_render" color="pink">
+
+            </data-list-render>
+        </div>
     </v-container>
 </template>
 
 <script>
+    import DataListRender from "../../../components/share/DataListRender";
+
     export default {
-        name: "OffenderView"
+        name: "OffenderView",
+        components: {DataListRender},
+        data() {
+            return {
+                offender: null,
+                offender_data_render: null,
+                offender_address_render: []
+            }
+        }, async created() {
+            await this.loadData()
+        }, methods: {
+            async loadData() {
+                this.offender = await this.$store.dispatch('offender/getUserDetail', this.$route.params.id)
+                this.data_preparing()
+            },
+            data_preparing() {
+                this.address_preparing()
+                this.offender_data_render = [
+                    {
+                        label: "ข้อมูลพื้นฐาน",
+                        data: [
+                            {
+                                key: "อายุ",
+                                value: this.offender.o_age
+                            }, {
+                                key: "เชื้อชาติ",
+                                value: this.offender.o_race
+                            }, {
+                                key: "สัญชาติ",
+                                value: this.offender.o_nationality
+                            }, {
+                                key: "การศึกษา",
+                                value: this.offender.o_education
+                            }, {
+                                key: "อาชีพ",
+                                value: this.offender.o_career
+                            }, {
+                                key: "ศาสนา",
+                                value: this.offender.o_religion
+                            }, {
+                                key: "สถานภาพ",
+                                value: this.offender.o_marital_status
+                            },
+                        ]
+                    }
+                ];
+            },
+            address_preparing() {
+                let data_list = this.offender.offenderaddress_set.reverse()
+                data_list.forEach(element => {
+                    let label = ""
+                    if (this.offender_address_render.length === 0) {
+                        label = "ที่อยู่"
+                    }
+                    this.offender_address_render.push({
+                        label: label,
+                        data: [
+                            {
+                                key: "บ้านเลขที่",
+                                value: element.o_address_house
+                            },
+                            {
+                                key: "หมู่บ้าน/ชุมชน",
+                                value: element.o_address_village
+                            },
+                            {
+                                key: "หมู่ที่",
+                                value: element.o_address_mu
+                            },
+                            {
+                                key: "ซอย",
+                                value: element.o_address_alley
+                            },
+                            {
+                                key: "ถนน",
+                                value: element.o_address_street
+                            },
+                            {
+                                key: "ตำบล",
+                                value: element.district ? element.district.name_th : null
+                            },
+
+                            {
+                                key: "อำเถอ",
+                                value: element.amphure ? element.amphure.name_th : null
+                            },
+                            {
+                                key: "จังหวัด",
+                                value: element.province ? element.province.name_th : null
+                            },
+                            {
+                                key: "รหัสไปรษณีย์",
+                                value: element.o_address_zip_code
+                            },
+                            {
+                                key: "สถานะ",
+                                value: element.o_address_status  ===  true ? 'ที่อยู่ปัจจุบัน' : 'ที่อยู่เก่า'
+                            },
+                        ]
+                    })
+                });
+            },
+
+        }
     }
 </script>
 
